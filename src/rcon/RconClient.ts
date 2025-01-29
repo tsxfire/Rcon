4029ce0fe2ff6cc9599b272cbf8267207ba78dfd
tsxfire <animeandrew18@gmail.com>
2025-01-29 15:33:40 -0600

    missing syntax

--- src/rcon/RconClient.ts
+++ src/rcon/RconClient.ts
@@ -20,7 +20,7 @@ export class RconClient extends EventEmitter {
           host: this.config.host,
           port: this.config.port,
           password: this.config.password,
-          rcon: true
+          rcon: true,
           connectTimeout: 5000
         });
 
